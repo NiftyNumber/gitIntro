37430d63b0560b569eea7f5c70e5e70a2385f871
lc <root931@hotmail.com>
2021-09-04 14:16:23 -0600

    Complete Initial Version

---
+++ gitIntro.sh
@@ -0,0 +1,10 @@
+# Initialize git
+git init
+
+
+# You can choose files in the Staging Area that you want to commit
+# See what files are in the git Staging Area
+git status
+
+# Add files to the Staging Area that you want to track changes
+git add gitIntro.sh
