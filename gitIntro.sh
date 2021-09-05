b68852a344df8c24b1683aed855d225689ab81a8
lc <root931@hotmail.com>
2021-09-05 10:15:57 -0600

    Add checkout command

--- gitIntro.sh
+++ gitIntro.sh
@@ -1,17 +1,22 @@
-# Initialize git
+#* Initialize git in the working directory.
 git init
 
 git config --global user.email "root931@hotmail.com"
 git config --global user.name "lc"
 
-# You can choose files in the Staging Area that you want to commit
+#* Staging Area (Middle Ground) is a location where the files that you want to commit should be added to.
+# You can choose files to add to Staging Area
+#* You might not want to track all files.
 # See what files are in the git Staging Area
 git status
 
-# Add files to the Staging Area that you want to track changes
+#* Add desired files to the Staging Area that you want to track changes
 git add gitIntro.sh
 
-# Commit changes
+#* Add all files in the Staging Area
+git add .
+
+#* Commit changes
 #! Commet in Present tense, you are doing it now
 git commit -m "Complete Initial Version"
 
@@ -19,3 +24,9 @@ git commit -m "Complete Initial Version"
 git log
 
 #* Unique commits are stored in hash: e.g. 37430d63b0560b569eea7f5c70e5e70a2385f871
+
+#* Check out the difference between curreent version and latest commit
+git diff gitIntro.sh
+
+#* Rollback (checkout) to previous commit
+git checkout gitIntro.sh
